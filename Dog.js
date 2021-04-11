6457c930837de8020f67f2da0ca81de71b70f997
NamPhung <namphungptit2204@gmail.com>
2021-04-11 14:37:08 +0700

    add fix sayHi() for dog

--- Dog.js
+++ Dog.js
@@ -3,14 +3,14 @@ var chalk =require('chalk')
 function Dog(name){
     this.name = name;
     this.stomach = [];
-}
+}  
 
 Dog.prototype.eat = function(Cat){
     this.stomach.push(Cat);
 }
 
 Dog.prototype.sayHi = function(){
-    console.log("Hi! My name is " + chalk.red(this.name));
+    console.log("Hi!I am a Dog, My name is " + chalk.red(this.name));
 }
 
 module.exports = Dog;
